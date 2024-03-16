c706ed48df551f52d610e85a8930ae814fe33ded
benhassan98 <habdala70@gmail.com>
2024-03-16 23:36:18 +0200

    added port and cacheSize extraction from cli

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,79 @@
+import server.Server;
+
+import java.util.Arrays;
+import java.util.Optional;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static Optional<Integer> getPort(String[] args){
+
+
+        int portIndex = Arrays.asList(args).indexOf("-p");
+        if(portIndex != -1 ){
+
+
+            if(portIndex + 1 < args.length){
+
+                try{
+                   Integer port = Integer.parseInt(args[portIndex + 1]);
+                   return Optional.of(port);
+                }
+                catch (NumberFormatException e){
+                    return Optional.empty();
+                }
+
+
+            }
+
+            return Optional.empty();
+        }
+
+        return Optional.of(11211);
+
+
+    }
+    public static Optional<Long> getCacheSizeInBytes(String[] args){
+
+        int sizeIndex = Arrays.asList(args).indexOf("-m");
+        if(sizeIndex != -1 ){
+
+
+            if(sizeIndex + 1 < args.length){
+
+                try{
+                    Long size = Long.getLong(args[sizeIndex + 1]) * 1024L;
+                    return Optional.of(size);
+                }
+                catch (NumberFormatException e){
+                    return Optional.empty();
+                }
+
+
+            }
+
+            return Optional.empty();
+        }
+
+        return Optional.of(1024L);
+
+    }
+
+    public static void main(String[] args){
+
+        var port = getPort(args);
+        var cacheSize = getCacheSizeInBytes(args);
+
+        if(port.isEmpty()){
+            System.out.println("Please Enter a valid Port");
+            return;
+        }
+        if(cacheSize.isEmpty()){
+            System.out.println("Please Enter a valid Size");
+            return;
+        }
+
+        Server server = new Server(cacheSize.get());
+        server.start(port.get());
+
     }
 }
---
+++ src/server/Server.java
@@ -0,0 +1,14 @@
+package server;
+
+public class Server {
+
+    private Long cacheSize;
+
+    public Server(Long cacheSize){
+        this.cacheSize = cacheSize;
+    }
+
+    public void start(Integer port){}
+
+
+}
